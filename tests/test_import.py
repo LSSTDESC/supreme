d835c4ddd20663cedd1b8a86573e888ec80f3f48
Eli Rykoff <erykoff@stanford.edu>
2020-05-01 10:07:47 -0700

    Write new workflow based on stack from docker

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,1 +1,11 @@
-import supreme  # noqa
+import unittest
+
+
+class ImportTestCase(unittest.TestCase):
+    def test_inport(self):
+        """
+        Test that we can import supreme.
+        """
+        import supreme  # noqa
+
+        pass
