236267532abb038ff9dfa833f82167fba295a900
Eli Rykoff <erykoff@stanford.edu>
2020-05-06 14:08:24 -0700

    Add tests that can run on CI.

---
+++ tests/supreme_test_base.py
@@ -0,0 +1,130 @@
+import unittest
+import os
+import numpy as np
+import numpy.testing as testing
+import shutil
+
+import healsparse
+
+
+class SupremeTestBase(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = None
+
+    def tearDown(self):
+        if self.test_dir is not None:
+            if os.path.exists(self.test_dir):
+                shutil.rmtree(self.test_dir, True)
+
+    def check_expected_maps_patch(self, expected_dict, tract, patch, filter_name):
+        """
+        Check for expected maps, ranges, types for a patch.
+
+        Parameters
+        ----------
+        expected_dict : `OrderedDict`
+           Ordered dictionary with keys of map names, values of [min, max].
+           The first item must be the patch input with [nccd, width_expected].
+        tract : `int`
+           Tract number
+        patch : `str`
+           Patch name
+        filter_name : `str`
+           Filter name
+        """
+        for em in expected_dict:
+            map_name = os.path.join(self.test_dir, 'testing_%05d_%s_%s_%s.hs'
+                                    % (tract, patch, filter_name, em))
+            self.assertTrue(os.path.isfile(map_name))
+            m = healsparse.HealSparseMap.read(map_name)
+
+            valid_pixels = m.valid_pixels
+            # The patch_inputs are special, and need to be first
+            if em == 'patch_inputs':
+                input_valid_pixels = valid_pixels
+                metadata = m.metadata
+
+                # Check the metadata
+                self.assertTrue(metadata['WIDEMASK'])
+                self.assertEqual(metadata['WWIDTH'], expected_dict[em][1])
+                nccd = 0
+                nvis = 0
+                nwt = 0
+                nslv = 0
+                nssg = 0
+                nbgm = 0
+                nbgv = 0
+                for i in range(10):
+                    if 'B%04dCCD' % (i) in metadata:
+                        nccd += 1
+                    if 'B%04dVIS' % (i) in metadata:
+                        nvis += 1
+                    if 'B%04dWT' % (i) in metadata:
+                        nwt += 1
+                    if 'B%04dSLV' % (i) in metadata:
+                        nslv += 1
+                    if 'B%04dSSG' % (i) in metadata:
+                        nssg += 1
+                    if 'B%04dBGM' % (i) in metadata:
+                        nbgm += 1
+                    if 'B%04dBGV' % (i) in metadata:
+                        nbgv += 1
+                self.assertEqual(nccd, expected_dict[em][0])
+                self.assertEqual(nvis, nccd)
+                self.assertEqual(nwt, nccd)
+                self.assertEqual(nslv, nccd)
+                self.assertEqual(nssg, nccd)
+                self.assertEqual(nbgm, nccd)
+                self.assertEqual(nbgv, nccd)
+            else:
+                # Make sure we have the same valid pixels as the input map
+                testing.assert_array_equal(valid_pixels, input_valid_pixels)
+
+                self.assertEqual(m.dtype.name, expected_dict[em][2])
+
+                self.assertGreater(np.min(m.get_values_pix(valid_pixels)),
+                                   expected_dict[em][0])
+                self.assertLess(np.max(m.get_values_pix(valid_pixels)),
+                                expected_dict[em][1])
+
+    def check_expected_maps_tract(self, expected_dict, tract, filter_name):
+        """
+        Check for expected maps, ranges, types for a tract.
+
+        Parameters
+        ----------
+        expected_dict : `OrderedDict`
+           Ordered dictionary with keys of map names, values of [min, max].
+           The first item must be the patch input with BLAH.
+        tract : `int`
+           Tract number
+        filter_name : `str`
+           Filter name
+        """
+        for em in expected_dict:
+            if em == 'patch_inputs':
+                for patch in expected_dict[em][1:]:
+                    map_name = os.path.join(self.test_dir, 'testing_%05d_%s_%s_%s.hs'
+                                            % (tract, patch, filter_name, em))
+
+                    self.assertTrue(os.path.isfile(map_name))
+                    m = healsparse.HealSparseMap.read(map_name)
+
+                    valid_pixels = m.valid_pixels
+
+                    self.assertGreater(valid_pixels.size, expected_dict[em][0])
+            else:
+                map_name = os.path.join(self.test_dir, 'testing_%05d_%s_%s.hs'
+                                        % (tract, filter_name, em))
+
+                self.assertTrue(os.path.isfile(map_name))
+                m = healsparse.HealSparseMap.read(map_name)
+
+                valid_pixels = m.valid_pixels
+
+                self.assertEqual(m.dtype.name, expected_dict[em][2])
+
+                self.assertGreater(np.min(m.get_values_pix(valid_pixels)),
+                                   expected_dict[em][0])
+                self.assertLess(np.max(m.get_values_pix(valid_pixels)),
+                                expected_dict[em][1])
---
+++ tests/test_patch_quick_rc2.py
@@ -0,0 +1,65 @@
+import unittest
+import os
+import tempfile
+from collections import OrderedDict
+
+import supreme
+
+import lsst.daf.persistence as dafPersist
+import lsst.utils
+
+import supreme_test_base
+
+
+class PatchQuickRc2TestCase(supreme_test_base.SupremeTestBase):
+    """
+    Tests for running a single patch, quick mode, with HSC RC2 data
+    """
+    @classmethod
+    def setUpClass(cls):
+        try:
+            cls.data_dir = lsst.utils.getPackageDir('supreme_testdata')
+        except LookupError:
+            raise unittest.SkipTest("supreme_testdata not setup")
+
+    def test_patch_quick(self):
+        """
+        Test a single RC2 patch in quick mode
+        """
+        repo = os.path.join(self.data_dir, 'supreme', 'testdata', 'RC2_test', 'rerun', 'coadd')
+        tract = 9697
+        filter_name = 'HSC-I'
+        patch = '2,2'
+
+        self.test_dir = tempfile.mkdtemp(dir='./', prefix='TestPatchHsc-')
+
+        butler = dafPersist.Butler(repo)
+        config = supreme.Configuration(os.path.join('configs/config_quick_rc2.yaml'))
+
+        mapper = supreme.PatchMapper(butler, config, self.test_dir)
+        mapper.run(filter_name, tract, patch, return_values_list=False)
+
+        # Check that everything is there
+        expected_dict = OrderedDict()
+        expected_dict['patch_inputs'] = [2, 1]
+        expected_dict['airmass_max'] = [1.15, 1.3, 'float64']
+        expected_dict['airmass_min'] = [1.15, 1.3, 'float64']
+        expected_dict['airmass_wmean'] = [1.15, 1.3, 'float64']
+        expected_dict['background_wmean'] = [690.0, 790.0, 'float64']
+        expected_dict['bgmean_wmean'] = [690.0, 790.0, 'float64']
+        expected_dict['exptime_sum'] = [199.0, 401.0, 'float64']
+        expected_dict['nexp_sum'] = [0, 3, 'int32']
+        expected_dict['psf_size_wmean'] = [1.15, 1.45, 'float64']
+        expected_dict['psf_e1_wmean'] = [0.0, 0.15, 'float64']
+        expected_dict['psf_e2_wmean'] = [-0.15, 0.15, 'float64']
+        expected_dict['skylevel_wmean'] = [690.0, 790.0, 'float64']
+        expected_dict['skysigma_wmean'] = [8.4, 9.0, 'float64']
+        expected_dict['coadd_image_mean'] = [-0.1, 15.0, 'float64']
+        expected_dict['coadd_variance_mean'] = [0.005, 0.03, 'float64']
+        expected_dict['coadd_mask_or'] = [-1, 54000, 'int32']
+
+        self.check_expected_maps_patch(expected_dict, tract, patch, filter_name)
+
+
+if __name__ == '__main__':
+    unittest.main()
